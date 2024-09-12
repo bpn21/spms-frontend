3cba04aec98b47b7be7fd4390040dfce6a0b63dc
bpn21 <bpngaire@gmail.com>
2024-09-13 01:03:10 +0545

    eslint.config updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,11 +4,11 @@ import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+  { files: ["**/*.{js,mjs,cjs,vue}"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  // ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
   {
     files: ["**/*.vue"],
