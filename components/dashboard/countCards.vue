a139fc5bd22969eb4ef85c01a94e1815c4446d51
bpn21 <bpngaire@gmail.com>
2025-05-10 22:53:57 +0545

    countCards for dashboard

---
+++ components/dashboard/countCards.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-bold q-pa-xs text-caption row q-gutter-sm">
+    <q-card class="bg-grey-4 q-pa-xs row justify-between col-grow"
+      >Total OVERDUE
+      <div class="text-right text-h3 text-bold text-red-8">1</div>
+    </q-card>
+    <q-card class="bg-grey-4 q-pa-xs row justify-between col-grow"
+      >Total DUES
+      <div class="text-right text-h3 text-bold text-red-4">4</div>
+    </q-card>
+    <q-card class="bg-grey-4 q-pa-xs row justify-between col-grow"
+      >Total PAID
+      <div class="text-right text-h3 text-bold text-green">90</div>
+    </q-card>
+  </div>
+</template>
+
+<script setup></script>
